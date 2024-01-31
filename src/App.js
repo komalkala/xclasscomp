d70839c391cd5da15e9670a217a8d8e3e0afb538
komalkala <komalkala42@gmail.com>
2024-01-31 14:46:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+class CounterApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+ cardStyle = {
+    margin: '10px',
+  };
+
+  render() {
+    return (
+      <div style={this.cardStyle}>
+        <h1>Counter Application</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
